c1398ea7e47804526623c1c1a9bfa5af6ebf6335
Ben Halverson <benhalverson33@gmail.com>
2023-11-29 00:18:59 -0800

    back to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "hybrid",
+  output: "static",
   adapter: vercel(
     {
       webAnalytics: {
